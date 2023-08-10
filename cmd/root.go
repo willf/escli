f153be72edfe8a7b156d827e22ab93bcf27f71fb
Will Fitzgerald <37049+willf@users.noreply.github.com>
2023-08-10 09:03:11 -0400

    wip

--- cmd/root.go
+++ cmd/root.go
@@ -12,6 +12,7 @@ import (
 )
 
 var cfgFile string
+var database string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -48,7 +49,7 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
--- lib/client.go
+++ lib/client.go
@@ -4,22 +4,44 @@ package lib
 
 import (
 	// elasticsearch
+	"errors"
+
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/spf13/viper"
 )
 
 // ElasticClient creates an Elasticsearch client
 func ElasticClient() (es *elasticsearch.Client, err error) {
-	server := viper.GetString("ELASTICSEARCH_SERVER")
+	servers := viper.GetStringSlice("ELASTICSEARCH_SERVERS")
 	user := viper.GetString("ELASTICSEARCH_USER")
 	password := viper.GetString("ELASTICSEARCH_PASSWORD")
 	certificate_fingerprint := viper.GetString("ELASTICSEARCH_CERTIFICATE_FINGERPRINT")
 	api_key := viper.GetString("ELASTICSEARCH_API_KEY")
+
+	//
+	if len(servers) == 0 {
+		err = errors.New("no Elasticsearch servers specified; config file must contain ELASTICSEARCH_SERVERS")
+		return
+	}
+	if user == "" {
+		err = errors.New("no Elasticsearch user specified; config file must contain ELASTICSEARCH_USER")
+		return
+	}
+	if password == "" {
+		err = errors.New("no Elasticsearch password specified; config file must contain ELASTICSEARCH_PASSWORD")
+		return
+	}
+	if certificate_fingerprint == "" {
+		err = errors.New("no Elasticsearch certificate fingerprint specified; config file must contain ELASTICSEARCH_CERTIFICATE_FINGERPRINT")
+		return
+	}
+	if api_key == "" {
+		err = errors.New("no Elasticsearch API key specified; config file must contain ELASTICSEARCH_API_KEY")
+		return
+	}
 	// create a new client
 	cfg := elasticsearch.Config{
-		Addresses: []string{
-			server,
-		},
+		Addresses:              servers,
 		Username:               user,
 		Password:               password,
 		CertificateFingerprint: certificate_fingerprint,
